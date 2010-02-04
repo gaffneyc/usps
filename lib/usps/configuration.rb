2c47281d55882bd00d4a03018e1db7c9c8bbd0a8
Chris Gaffney <cgaffney@crayoninterface.com>
2010-02-04 15:01:33 -0500

    Add some documentation to the config class.

--- lib/usps/configuration.rb
+++ lib/usps/configuration.rb
@@ -1,9 +1,16 @@
 module USPS
+  # Configuration options:
+  #   username: Provided by the USPS during registration
+  #   timeout: How many seconds to wait for a response before raising Timeout::Error
+  #   testing: Should requests be done against the testing service or not
+  #            (only specific requests are supported).
   class Configuration < Struct.new(:username, :timeout, :testing)
     def initialize
       self.timeout  = 5
       self.testing  = false
       self.username = ENV['USPS_USER']
     end
+
+    alias :testing? :testing
   end
 end
